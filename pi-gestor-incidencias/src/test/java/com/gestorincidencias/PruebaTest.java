8b2429a18fb04dd143e78640472b2f88899645cf
JosepDurba <71144851+josepdurba@users.noreply.github.com>
2026-05-26 13:11:25 +0200

    Add initial unit test for PruebaTest class

---
+++ pi-gestor-incidencias/src/test/java/com/gestorincidencias/PruebaTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PruebaTest {
+
+    @Test
+    void testSimple() {
+        assertTrue(true);
+    }
+}
